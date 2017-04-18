e4165a40bc28db842e75a7251e53ff2ed00e331c
Amanda <acheng742@gmail.com>
2017-04-18 17:28:34 -0400

    add custom JS for mouse enter/out img change

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//JQuery Plugin Codes
 $(document).ready(function($) {
 	// This plugin allows different phrases to change in between a string
 	$("#js-rotating").Morphext({
@@ -15,4 +16,16 @@ $(document).ready(function($) {
 	$('.automatic-slider').unslider({
 		autoplay: true
 	});
+});
+
+// Custom JavaScript Code
+
+var customImage = document.getElementById ('custom');
+
+customImage[0].addEventListener( "mouseover", function(){ //listens for mouse over on image
+	customImage[0].src = "img/yum.jpg"; //changea image
+});
+
+mainImage[0].addEventListener( "mouseout", function(){ //listens for mouse out on image
+	customImage[0].src = "img/ramen.jpg"; //changea image back
 });
