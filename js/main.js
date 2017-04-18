2c2e782b19ad8e0f5d3f38ea764c53f49f251a40
Amanda <acheng742@gmail.com>
2017-04-18 16:47:32 -0400

    modify styling for jquery unslide plugin

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
-// This plugin allows different phrases to change in between a string
-$("#js-rotating").Morphext({
-    // The [in] animation type.
-    animation: "bounceIn",
-    // An array of phrases to rotate are created based on this separator
-    separator: ",",
-    // The delay between the changing of each phrase in milliseconds.
-    speed: 2000,
-    complete: function () {
-        // Called after the entrance animation is executed.
-    }
-});
-//This plugin allows images to slide in a carousel
 $(document).ready(function($) {
-			$('.my-slider').unslider();
-		});
+	// This plugin allows different phrases to change in between a string
+	$("#js-rotating").Morphext({
+	    // The [in] animation type.
+	    animation: "bounceIn",
+	    // An array of phrases to rotate are created based on this separator
+	    separator: ",",
+	    // The delay between the changing of each phrase in milliseconds.
+	    speed: 2000,
+	    complete: function () {
+	        // Called after the entrance animation is executed.
+	    }
+	});
+	//This plugin allows images to slide in a carousel
+	$('.automatic-slider').unslider({
+		autoplay: true
+	});
+});
