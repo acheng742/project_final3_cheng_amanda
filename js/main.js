90e05db91220dd5acdf0dd61bf97024043626984
Amanda <acheng742@gmail.com>
2017-04-19 01:32:25 -0400

    modify semantic html5 element for proper outlining

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-//JQuery Plugin Codes
-$(document).ready(function($) {
+//jQuery Codes
+$(document).ready(function() {
+	// Custom jQuery code where the element's content changes when the button is clicked
+	$('.next').click(function(){
+		$('.yes').text('Then what are you waiting for?!');
+	});
 	// This plugin allows different phrases to change in between a string
 	$("#js-rotating").Morphext({
-	    // The [in] animation type.
 	    animation: "bounceIn",
-	    // An array of phrases to rotate are created based on this separator
 	    separator: ",",
-	    // The delay between the changing of each phrase in milliseconds.
 	    speed: 2000,
 	    complete: function () {
-	        // Called after the entrance animation is executed.
 	    }
 	});
 	//This plugin allows images to slide in a carousel
@@ -21,10 +21,10 @@ $(document).ready(function($) {
 // Custom JavaScript Code
 
 var customImage = document.getElementById ('custom');
-customImage.addEventListener( "mouseover", function(){ //listens for mouse over on image
-	customImage.src = "img/yum.jpg"; //changes image
+customImage.addEventListener( 'mouseover', function(){ //listens for mouse over on image
+	customImage.src = 'img/yum.jpg'; //changes image
 });
 
-customImage.addEventListener( "mouseout", function(){ //listens for mouse out on image
-	customImage.src = "img/ramen.jpg"; //changes image back to original
+customImage.addEventListener( 'mouseout', function(){ //listens for mouse out on image
+	customImage.src = 'img/ramen.jpg'; //changes image back to original
 });
